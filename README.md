b1880e1a55ab220725c1b0f5d2a10a20e5830ae5
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-10-12 09:34:25 +0100

    Random-tokens-using-python

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tokens using python
+
+* This code snippet uses the random.randint(1000, 9999) function to generate four random integers between 1000 and 9999, ensuring they have exactly four digits.
+
+* The generated integers will be stored in the random_integers list.
---
+++ tokens.py
@@ -0,0 +1,9 @@
+import random
+
+random_integers = []
+
+for _ in range(4):
+    random_integer = f"{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}"
+    random_integers.append(random_integer)
+
+# random_integers now contains a list of 4 random 4-digit integers separated by hyphens
