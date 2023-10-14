9c858196b2d63a7e3cf26f6c7b933fcdd3c242df
Adetunjieliazer <adetunjieliazer@gmail.com>
2023-10-14 22:52:00 +0100

    Random-tokens-using-python

--- tokens.py
+++ tokens.py
@@ -2,8 +2,8 @@ import random
 
 random_integers = []
 
-for _ in range(4):
-    random_integer = f"{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}"
+for _ in range(1):
+    random_integer = f"{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}-{random.randint(1000, 9999)}"
     print(random_integer)
 
 # random_integers now contains a list of 4 random 4-digit integers separated by hyphens
